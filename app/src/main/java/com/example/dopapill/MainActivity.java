ece3aa2a3ef0b6565362742e0314da6ce0f19f45
Rokayia <rokayia.almhdie@gmail.com>
2019-02-09 21:58:40 +0100

    goodbye button works as specified

--- app/src/main/java/com/example/dopapill/MainActivity.java
+++ app/src/main/java/com/example/dopapill/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.dopapill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mGreeting;
+    private Button nGoodByeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nGoodByeButton=(Button) findViewById(R.id.goodbye_button);
+        mGreeting=(TextView)findViewById(R.id.greeting);
+        nGoodByeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                nGoodByeButton.setVisibility(View.INVISIBLE);
+                mGreeting.setVisibility(View.INVISIBLE);
+            }
+        };
     }
 }
